fa698121fb31ba92a146eafcc88f777491652a1f
Mathieu Cartoixa <mathieu.cartoixa@free.fr>
2020-04-13 10:30:43 +0200

    Implement the force-user-list task

---
+++ src/test/com/mcartoixa/ant/sfdx/force/user/ListTaskTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2020 Mathieu Cartoixa.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mcartoixa.ant.sfdx.force.user;
+
+import org.apache.tools.ant.BuildFileRule;
+import org.apache.tools.ant.Project;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ *
+ * @author Mathieu Cartoixa
+ */
+public class ListTaskTest {
+
+    @Rule
+    public final BuildFileRule buildRule = new BuildFileRule();
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    public ListTaskTest() {
+    }
+
+    @Before
+    public void setUp() {
+        buildRule.configureProject("src/test/com/mcartoixa/ant/sfdx/force/user/list.xml", Project.MSG_DEBUG);
+    }
+
+    @Test
+    public void executeShouldSetStatusProperty() {
+        buildRule.executeTarget("execute");
+        Assert.assertEquals("Status property should be set", "0", buildRule.getProject().getProperty("execute.status"));
+    }
+
+    @Test
+    public void executeShouldAddJsonArgument() {
+        buildRule.executeTarget("execute");
+        Assert.assertTrue("Full log should contain --json argument", buildRule.getFullLog().contains("'--json'"));
+    }
+
+    @Test
+    public void executeShouldAddTargetDevHubUsernameArgument() {
+        buildRule.executeTarget("execute");
+        Assert.assertTrue("Full log should contain -v argument", buildRule.getFullLog().contains("'-vtestdevhub'"));
+    }
+
+    @Test
+    public void executeShouldAddTargetUsernameArgument() {
+        buildRule.executeTarget("execute");
+        Assert.assertTrue("Full log should contain -u argument", buildRule.getFullLog().contains("'-utestuser'"));
+    }
+
+    @Test
+    public void executeShouldLogUser() {
+        buildRule.executeTarget("execute");
+        Assert.assertTrue("User should be logged " + buildRule.getLog(), buildRule.getLog().contains("test@ant-sfdx.org"));
+    }
+
+    @Test
+    public void executeQuietShouldHaveNoOutput() {
+        buildRule.executeTarget("execute-quiet");
+        Assert.assertTrue("Log should be empty", buildRule.getLog().isEmpty());
+    }
+}
